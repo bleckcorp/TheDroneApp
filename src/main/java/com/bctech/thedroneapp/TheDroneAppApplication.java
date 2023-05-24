14eee349096070da941b6284d6db6abd43a0e7ec
Blessing Chukwubuogum <59502310+bleckcorp@users.noreply.github.com>
2023-05-24 11:13:12 +0100

    The origin of all commits

---
+++ src/main/java/com/bctech/thedroneapp/TheDroneAppApplication.java
@@ -0,0 +1,13 @@
+package com.bctech.thedroneapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheDroneAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheDroneAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bctech/thedroneapp/TheDroneAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bctech.thedroneapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheDroneAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
